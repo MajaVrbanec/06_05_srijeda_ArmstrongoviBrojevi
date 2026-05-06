48bf3edfc15ffe6ad2afba49dff00e2027f95977
Maja <majavrbanec94@gmail.com>
2026-05-06 20:52:48 +0200

    Armstrongovi brojevi

--- src/ArmstrongoviBrojevi.java
+++ src/ArmstrongoviBrojevi.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class ArmstrongoviBrojevi {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10000; i++) {
+            int privremeniBroj = i;
+            int suma = 0;
+            int ostatak = 0;
+
+            while (privremeniBroj > 0) {
+                ostatak = privremeniBroj % 10;
+                suma = suma + (ostatak * ostatak * ostatak);
+                privremeniBroj = privremeniBroj / 10;
+
+            }
+            if (suma == i) {
+                System.out.println("Ispiši Armstrongove brojeve manje od 10000: " + suma);
+            }
+
+        }
+
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a = 52;
+        int b = 10;
+
+        System.out.println("Dijeljenje: " + (double)a/b); // int u double rezultat
+        System.out.println("Cjelobrojno dijeljenje: " + a / b); //Cjelobrojno djeljenje -> sa 2 kose linije //
+        System.out.println("Dijeljenje s ostatkom :" + a % b); // ako nas zanima ostatak od dijeljenja
+
+        //int broj = 521;
+        // 521 : 10 = 52.1  (double)
+        // cjelobrojno dijeljenje: 521 / 10 = 52
+        // s ostatkom 521 % 10 = 1 --> jer je 52 * 10 = 521 i ostatak je 1
+
+
+        //Zadatak: Unijeti broj kroz konzolu i računamo sumu znamenki toj broja
+        // Koraci:
+        //1. tražiti od korisnika da unese broj (Koristimo skener i spremanje broja)
+        //2. Kreirati potrebnih pomoćnih varijabli (jedna za sumu znamenki, spremanje privremenog broja..)
+        //3. sve to pospremiti unutar while petlje -> prekid izvođenja petlje je kada dobijemo broj 0
+        // 4. ispisati broj znamenki
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int suma = 0; // kutija u koju spremamo rezultat tj. ostatak
+        System.out.println("Unesi broj: ");
+        int broj = sc.nextInt(); // broj koji ćemo unijeti u konzolu
+        int ostatak = 0; // pomoćna varijabla ostatak - privremeni spremnik u koji stavljamo zadnju znamenku prije zbrajanja u sumu
+//while (broj > 0) -> radi sve dok u broju ima znamenki, tj. ponavljaj sve dok ne potrošimo (izbrišemo) sve znamenke iz broja
+        while ( broj > 0) { //petlja while kroz koju provjeravamo uvjet da je broj koji dijelimo veći od 0
+            ostatak = broj % 10; // dijelimo s % da dobijemo ostatak, čupamo zadnju znamenku broja?
+            suma += ostatak; // suma = suma + ostatak (dodajemo tu znamnku u ukupnu sumu)
+            broj = broj / 10; //brišemo zadnju znamenku broja
+
         }
+        System.out.println("Ispiši broj znamenki: " + suma); //ispis zbroja znamenki iz kutije suma
+
     }
+
 }
